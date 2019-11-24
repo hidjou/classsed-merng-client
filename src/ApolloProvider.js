9a8ea693bff125651477d2cefaecb2ac8378fb87
Ahmed Hadjou <thehidjou@yahoo.com>
2019-11-24 13:23:59 +0000

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://quiet-reaches-09436.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
